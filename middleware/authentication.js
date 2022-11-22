6f740bb87631329b13f5b213b960e63c82061515
dewll <sodiqb86@gmail.com>
2022-11-22 19:57:09 +0100

    commited

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,13 +1,15 @@
 import  jwt from 'jsonwebtoken'
-//const jwt = require 'jsonwebtoken'
+import { config } from '../configs/appConfig.js';
+
+const {secret:{signingKey}} = config
 
 export function authenticatedToken(req,res, next){
     const authHeader = req.headers['authorization']
     console.log(authHeader)
     const token = authHeader && authHeader.split(' ')[1]
-    if (token == null) return res.status(403).json({message:"Token can't be empty"})
+    if (token == null) return res.status(401).json({message:"Token can't be empty"})
 
-    jwt.verify(token, process.env.TOKEN_SECRET, (err,user) => {
+    jwt.verify(token, signingKey, (err,user) => {
         if (err) return res.status(403).json({message:"Invalid Token"})
         req.user = user
 
